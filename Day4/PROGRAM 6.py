52d51d70bf0dcb005af9bd9063bb2271c223bb18
Sunidhi Singh <63974589+Sunidhi-A@users.noreply.github.com>
2020-06-16 23:57:47 +0530

    Update PROGRAM 6.py

--- Day4/PROGRAM 6.py
+++ Day4/PROGRAM 6.py
@@ -1,16 +1,36 @@
-def solve(s):  
-    t = ""
-    s = s.strip() + " "
-    print(s)
-    for x in range(len(s)):
-        if s[x] == " ":
-            s[x+1] = s[x+1].upper()
-            t = t+s[x]
-            print(t)
-        else:
-            t = t+s[x]
-    s = t.strip()
-    return s
+size = int(input("Enter the no of words you want to add in dictonary: "))
+dict = []
+for i in range(size):
+    dict.append(input("Enter the word " + str(i + 1) + ": "))
+size = int(input("Enter the no of character you want to add in array: "))
+array = []
+result = []
+print("Enter the characters in array one by one ")
+for i in range(size):
+    array.append(input())
+for word in dict:
+    if set(word).issubset(set(array)):
+        result.append(word)
+print(", ".join(result) + ".")
 
 
-solve("Sunidhi is a good girl")
+def charCount(word):
+    dict = {}
+    for i in word:
+        dict[i] = dict.get(i, 0) + 1
+    return dict
+
+
+def possible_words(lwords, charSet):
+    for word in lwords:
+        flag = 1
+        chars = charCount(word)
+        for key in chars:
+            if key not in charSet:
+                flag = 0
+            else:
+                if charSet.count(key) != chars[key]:
+                    flag = 0
+        if flag == 1:
+            print(word)
+
